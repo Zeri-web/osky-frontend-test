4c1f845797de2ece1a6d9f2f44eaf41d1498fb1e
Zeri-web <arzeri66@gmail.com>
2022-05-31 12:16:20 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function preventProp(e){
+  if(confirm("Are you sure you want to open this link ?")){
+    return true;
+  }
+  else{
+    e.preventDefault();
+  }
+}
